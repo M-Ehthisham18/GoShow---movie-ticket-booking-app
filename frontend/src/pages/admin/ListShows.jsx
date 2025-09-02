f5ca13cb95902f5bbc33d57f4637c6ac13bee921
M-Ehthisham18 <ehthishamulhaq18@gmail.com>
2025-09-03 00:03:59 +0530

    appcontext file type

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Loader from '../../components/Loader';
 import Title from '../../components/admin/Title';
 import { dateFormate } from '../../lib/dateFormate.js';
-import { useAppContext } from '../../context/appContext';
+import { useAppContext } from '../../context/AppContext';
 import toast from 'react-hot-toast';
 
 const ListShows = () => {
